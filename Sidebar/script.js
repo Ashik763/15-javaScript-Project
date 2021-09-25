d98585d56dc75b3b4abe5e6e566bfa2cfa081697
Ashik Ghosh <ashikghosh763@gmail.com>
2021-09-25 10:01:52 +0600

    everything is done

---
+++ Sidebar/script.js
@@ -0,0 +1,7 @@
+function sidebarOpener(){
+    // console.log("clicked");
+    let sidebar =  document.getElementById("sidebar");
+    // console.log(sidebar);
+    sidebar.classList.toggle("toggler");
+}
+
