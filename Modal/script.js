d553eee64f763b38c51761f8aedf476ceefdc607
Ashik Ghosh <ashikghosh763@gmail.com>
2021-09-26 13:22:18 +0600

    Modal done deleted all console.log

--- Modal/script.js
+++ Modal/script.js
@@ -1,18 +1,9 @@
 var modal = document.getElementById("modal-content");
 
 
-// window.onclick = function(event) {
-//    // 
-
-//    if (modal.classList.contains("tog")) {
-//       // console.log();
-//       modal.style.display = "none";
-//    }
-//  }
-
 
 function modalOpen(){
-   console.log("clicked");
+
    let modal = document.getElementById("modal-content");
    modal.classList.toggle("tog");
    
