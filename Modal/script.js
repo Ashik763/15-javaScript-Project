33039d3f96aaec425763c58dd866409bbf24e177
Ashik Ghosh <ashikghosh763@gmail.com>
2021-09-26 13:20:48 +0600

    Modal done

---
+++ Modal/script.js
@@ -0,0 +1,20 @@
+var modal = document.getElementById("modal-content");
+
+
+// window.onclick = function(event) {
+//    // 
+
+//    if (modal.classList.contains("tog")) {
+//       // console.log();
+//       modal.style.display = "none";
+//    }
+//  }
+
+
+function modalOpen(){
+   console.log("clicked");
+   let modal = document.getElementById("modal-content");
+   modal.classList.toggle("tog");
+   
+ 
+}
