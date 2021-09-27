71fe56df5513b5df15a7f43b1f9ef50076dd9fc8
Ashik Ghosh <ashikghosh763@gmail.com>
2021-09-28 03:38:17 +0600

    final done

--- Q & A/script.js
+++ Q & A/script.js
@@ -1,35 +1,44 @@
-function boxInfo(event){
-    // console.log(event.target);
-    let box =event.target;
+window.onload = function() {
+    let boxes = document.getElementsByClassName("box");
+    for(let i = 0; i < boxes.length; i++){
+       boxes[i].addEventListener("click", function(e) {
+       
+           boxInfo(e.currentTarget)
+       })
+    }
+    }
+    //
+
+
+
+function boxInfo(target){
+    
+   
+   let box1= target.children[0];
+   
+    let box =box1.children[0];
+    
     let iconDiv = box.parentElement;
     let boxDiv = iconDiv.parentElement;
     let one = boxDiv.parentElement;
-    //console.log(one);
+    
     let borderBottom = one.children[0].children;
-   // console.log(borderBottom[0]);
-    borderBottom[0].classList.toggle("border-bottom")
-    one.classList.toggle("oneWithAnswer");
-    let answer = boxDiv.parentElement.nextElementSibling; 
    
-    answer.classList.toggle("toggler");
-    // answer.classList.toggle("answer");
-    // console.log(answer);
-    // console.log(box);
-    // console.log(iconDiv);
-    // console.log(boxDiv);
-    // let div = box.children;
-    // console.log(div);
-    // let icon = div.classList.contains("fa-plus");
-    // console.log(icon.classList.contains("fa-plus"));
+    borderBottom[0].classList.toggle("border-bottom")
+    
+   
+    one.classList.toggle("oneWithAnswer")
+    one.nextElementSibling.classList.toggle("toggler");
+   
     if(box.classList.contains("fa-plus")){
-       // console.log("entered");
+       
         boxDiv.innerHTML = "";
         boxDiv.innerHTML =  `<div class="icon">
         <i  class="fas fa-minus"></i>
         </div>`;
     }
     else if(box.classList.contains("fa-minus")){
-       // console.log("entered2");
+       
         boxDiv.innerHTML = "";
         boxDiv.innerHTML =  `<div class="icon">
         <i  class="fas fa-plus"></i>
@@ -44,7 +53,7 @@ function boxInfo(event){
 
 function boxRotate(event){
     
-    // let box = document.getElementsByClassName("box")[0];
+  
     let box =event.target;
     
 
@@ -61,9 +70,4 @@ function boxRotate1(event) {
 
 }
 
-  // console.log(div);
-    // let icon = div[1].childNodes;
-    // console.log(icon);
-    //
-    // console.log(box);
-    // console.log(box.classList.contains("plus"));
+ 
